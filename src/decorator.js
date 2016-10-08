28a6859bd42b1a7b52f87739faf4a0876461595d
Yusinto Ngadiman <yusinto@gmail.com>
2016-10-08 21:35:01 +1100

    Fixed bug where camelCase is missing from imports.

--- src/decorator.js
+++ src/decorator.js
@@ -1,4 +1,5 @@
 import React, {PropTypes, Component} from 'react';
+import {camelCase} from 'lodash/camelCase';
 import {setFlags} from './actions';
 
 export default (flags) => (WrappedComponent) => {
