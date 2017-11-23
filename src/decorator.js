7881543b863b7ed4a187b948e4d68459e8b09627
Will Po <will.po@qantasassure.com>
2017-11-23 12:18:12 +1100

    Renamed variable flagsInitialised

--- src/decorator.js
+++ src/decorator.js
@@ -41,12 +41,12 @@ export default (flags) => (WrappedComponent) => {
       // If the flags have been retrieved on the server side, then we don't need
       // to re-retrieve them on the client side. If we do, the flag settings
       // would be overridden with the defaults set on the client.
-      const isFeatureFlagsRetrieved = Object.entries(featureFlags).length > 0;
+      const flagsInitialised = Object.entries(featureFlags).length > 0;
 
       for (const flag in flags) {
         const camelCasedKey = camelCase(flag);
 
-        if (!isFeatureFlagsRetrieved) {
+        if (!flagsInitialised) {
             flagValues[camelCasedKey] = ldClient.variation(flag, flags[flag]);
         }
 
@@ -58,7 +58,7 @@ export default (flags) => (WrappedComponent) => {
         });
       }
 
-      if (!isFeatureFlagsRetrieved) {
+      if (!flagsInitialised) {
           dispatch(setFlags(flagValues));
       }
     }
