73fc782726dc4d739c307e1a674ece0db7f3f819
Will Po <will.po@qantasassure.com>
2017-11-28 15:08:30 +1100

    Fix issue where when no flags are set on server, flags dont get initialised at all

--- src/decorator.js
+++ src/decorator.js
@@ -38,15 +38,10 @@ export default (flags) => (WrappedComponent) => {
       const {LD} = getState();
       const {isLDReady, ...featureFlags} = LD;
 
-      // If the flags have been retrieved on the server side, then we don't need
-      // to re-retrieve them on the client side. If we do, the flag settings
-      // would be overridden with the defaults set on the client.
-      const flagsInitialised = Object.entries(featureFlags).length > 0;
-
       for (const flag in flags) {
         const camelCasedKey = camelCase(flag);
 
-        if (!flagsInitialised) {
+        if (!featureFlags[camelCasedKey]) {
             flagValues[camelCasedKey] = ldClient.variation(flag, flags[flag]);
         }
 
@@ -58,9 +53,7 @@ export default (flags) => (WrappedComponent) => {
         });
       }
 
-      if (!flagsInitialised) {
-          dispatch(setFlags(flagValues));
-      }
+      dispatch(setFlags(flagValues));
     }
 
     render() {
