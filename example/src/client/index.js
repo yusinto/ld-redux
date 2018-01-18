9b534062b665832a66bb151bdc22905a9c19dbe7
Yus <yusinto@gmail.com>
2018-01-18 11:10:33 +1100

    Client index changes

--- example/src/client/index.js
+++ example/src/client/index.js
@@ -4,15 +4,20 @@ import {Router, browserHistory} from 'react-router';
 import routes from '../universal/routes';
 import {Provider} from 'react-redux';
 import createStore from '../universal/redux/store';
-import ldRedux from 'ld-redux';
+import ldRedux from '../../../lib';
+import flags from '../../flags.json';
 
 const reduxState = window.__INITIAL_STATE__ || undefined;
-const store = createStore(reduxState);
+const reduxStore = createStore(reduxState);
 
-ldRedux.init('your-client-side-id', store);
+ldRedux.init({
+  clientSideId: 'your-client-side-id',
+  reduxStore,
+  flags,
+});
 
 render(
-  <Provider store={store}>
+  <Provider store={reduxStore}>
     <Router routes={routes} history={browserHistory}/>
   </Provider>,
   document.getElementById('reactDiv')
